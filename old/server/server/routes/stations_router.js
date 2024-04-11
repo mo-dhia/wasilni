0f431da6b7723088a0f56507c9f9976f210f46f7
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2024-04-11 12:09:38 +0100

    deleted old [deployment size issue]

--- old/server/server/routes/stations_router.js
+++
@@ -1,9 +0,0 @@
-const stationRouter = require('express').Router();
-const stationController = require('../controllers/stations_controller')
-
-
-stationRouter.get('/stations', stationController.getAll);
-stationRouter.get('/stations/nearby/:latitude/:longitude', stationController.getNear);
-
-
-module.exports = stationRouter;
