c8480f6661cbcbe9458bf226f59e963ce656f397
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2024-08-06 16:51:57 +0100

    buttons

--- components/pages/home/sectionBot/sectionBot.jsx
+++ components/pages/home/sectionBot/sectionBot.jsx
@@ -5,6 +5,9 @@ import states from '../../../../store/states.js';
 import MapSVG from '../../../SVGs/map.svg.jsx';
 import s from './sectionBot.styles';
 import { sectionBotInit } from './sectionBot.func.js';
+import BusSVG from '../../../SVGs/bus.svg.jsx';
+import TrainSVG from '../../../SVGs/train.svg.jsx';
+import MetroSVG from '../../../SVGs/metro.svg.jsx';
 
 export default function SectionBot({ search, toggleExpand }) {
     const { VW, VH } = states();
@@ -29,6 +32,20 @@ export default function SectionBot({ search, toggleExpand }) {
                         <MapSVG width={VW * 0.065} height={VW * 0.065} />
                     </View>
                 </Animated.View>
+                <View style={{
+                    width: '100%', height: '5%', flexDirection: 'row', justifyContent: 'center', gap: VW * 0.15,
+                    marginTop: '7.5%',
+                }}>
+                    <View style={{ height: 50, width: 60, backgroundColor: '#0E887D', justifyContent: 'center', alignItems: 'center', borderRadius: VW * 0.05 }}>
+                        <BusSVG width={50} height={50} />
+                    </View>
+                    <View style={{ height: 50, width: 60, backgroundColor: '#0E887D', justifyContent: 'center', alignItems: 'center', borderRadius: VW * 0.05 }}>
+                        <TrainSVG width={50} height={50} />
+                    </View>
+                    <View style={{ height: 50, width: 60, backgroundColor: '#0E887D', justifyContent: 'center', alignItems: 'center', borderRadius: VW * 0.05 }}>
+                        <MetroSVG width={50} height={50} />
+                    </View>
+                </View>
             </Animated.View>
         </TouchableWithoutFeedback>
     );
